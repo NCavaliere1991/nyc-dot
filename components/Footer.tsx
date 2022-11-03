9f22c4b10e680db544b36619f2415518f8e67b86
Nick Cavaliere <nickcavaliere1991@gmail.com>
2022-11-02 20:46:27 -0400

    fix margins

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black p-4 text-center text-white mt-10 lg:mt-0">
+    <footer className="bg-black p-4 text-center text-white lg:mt-0">
       &copy; {new Date().getFullYear()} copyright. All rights reserved
     </footer>
   )
